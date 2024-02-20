5c388a98453af30586785d3d68a502f2f128cc71
Michael Boiko <mchlboiko@gmail.com>
2024-02-20 18:38:29 +0300

    added chroma library and click event

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
 const cols = document.querySelectorAll('.col');
 
-function generateColor(){
-    const hexCodes = '1234567890ABCDEF';
-    color = '';
-    for (let i = 0; i < 6; i++){
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+// function generateColor(){
+//     const hexCodes = '1234567890ABCDEF';
+//     color = '';
+//     for (let i = 0; i < 6; i++){
+//         color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+//     }
+//     return '#' + color;
+// }
+
+document.addEventListener('click', (event) => {
+    const dataType = event.target.dataset.type;
+    if(dataType === 'lock'){
+        if(event.target.tagName.toLowerCase() === 'i'){
+            node = event.target;
+        } else {
+            node = event.target.children[0];
+        }
+        node.classList.toggle('fa-lock-open');
+        node.classList.toggle('fa-lock');
     }
-    return '#' + color;
-}
+})
+
+document.addEventListener('keydown', (event) => {
+    if(event.code === 'Space'){
+        setRandomColors();
+    }
+})
 
 function setRandomColors(){
     cols.forEach(col => {
-        col.style.backgroundColor = generateColor();
+        const isLocked = col.querySelector('i').classList.contains('fa-lock');
+        const text = col.querySelector('h2');
+        const button = col.querySelector('button');
+        // const color = generateColor();
+        const color = chroma.random();
+        if(isLocked){
+            return
+        }
+        text.textContent = color;
+        col.style.backgroundColor = color;
+        setColorText(text, color);
+        setColorText(button, color);
     })
+}
+
+function setColorText(text, color){
+    const luminance = chroma(color).luminance();
+    text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
 setRandomColors();
